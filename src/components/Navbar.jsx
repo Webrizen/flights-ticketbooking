57f300c017d51c4768bcf4c7a13e9765480fabcf
WebRizen <webrizen@gmail.com>
2023-09-03 14:49:48 +0530

    now it's done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar() {
   return (
     <>
-      <header className="body-font sticky top-0 z-50">
+      <header className="body-font sticky top-0 z-50 backdrop-blur-xl">
         <div className="container mx-auto flex p-4 flex-row items-center justify-between">
           <img src="/logo.png" alt="FOC Logo" className="w-auto md:h-12 h-6" />
         </div>
